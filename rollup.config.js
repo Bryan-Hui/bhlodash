a3474b581a8b9563ff98e8a4ac028cf188724902
BryanHui <928102064@qq.com>
2024-05-26 16:16:54 +0800

    fix: package.json type

--- rollup.config.js
+++ rollup.config.js
@@ -1,67 +1,67 @@
-const path = require('path')
-const ts = require('rollup-plugin-typescript2');
-const nodeResolve = require('@rollup/plugin-node-resolve');
-const serve = require('rollup-plugin-serve');
-const { babel } = require('@rollup/plugin-babel');
-const pkg = require('./package.json');
-const commonjs = require('@rollup/plugin-commonjs')
-let esbuild = require('rollup-plugin-esbuild')
-esbuild = esbuild.default ?? esbuild;
+// const path = require('path')
+// const ts = require('rollup-plugin-typescript2');
+// const nodeResolve = require('@rollup/plugin-node-resolve');
+// const serve = require('rollup-plugin-serve');
+// const { babel } = require('@rollup/plugin-babel');
+// const pkg = require('./package.json');
+// const commonjs = require('@rollup/plugin-commonjs')
+// let esbuild = require('rollup-plugin-esbuild')
+// esbuild = esbuild.default ?? esbuild;
 
-const banner = '//  Ramda v' + pkg.version + '\n'
-    + '//  https://github.com/ramda/ramda\n'
-    + '//  (c) 2023-' + new Date().getFullYear() + ' Bryan Hui\n'
-    + '//  Ramda may be freely distributed under the MIT license.\n';
+// const banner = '//  Ramda v' + pkg.version + '\n'
+//     + '//  https://github.com/ramda/ramda\n'
+//     + '//  (c) 2023-' + new Date().getFullYear() + ' Bryan Hui\n'
+//     + '//  Ramda may be freely distributed under the MIT license.\n';
 
-const config = {
-    input: './src/index.ts',
-    output: {
-        file: path.resolve(__dirname, 'dist/bundle.js'),
-        format: 'umd',
-        // name: 'L',
-        // exports: 'named',
-        banner: banner
-        // global: 弄个全局变量来接收
-        // cjs: module.exports
-        // esm: export default
-        // iife: ()()
-        // umd: 兼容 amd + commonjs 不支持es6导入
-        // dir: path.resolve(__dirname, 'dist')
-        // dir: 'dist',
-        // sourcemap: true,
-        // format: 'umd',
-        // name: 'R',
-        // exports: 'named',
+// const config = {
+//     input: './src/index.ts',
+//     output: {
+//         file: path.resolve(__dirname, 'dist/bundle.js'),
+//         format: 'umd',
+//         // name: 'L',
+//         // exports: 'named',
+//         banner: banner
+//         // global: 弄个全局变量来接收
+//         // cjs: module.exports
+//         // esm: export default
+//         // iife: ()()
+//         // umd: 兼容 amd + commonjs 不支持es6导入
+//         // dir: path.resolve(__dirname, 'dist')
+//         // dir: 'dist',
+//         // sourcemap: true,
+//         // format: 'umd',
+//         // name: 'R',
+//         // exports: 'named',
 
-    },
+//     },
 
-    plugins: [
-        babel({
-            babelHelpers: 'bundled',
-            presets: [['@babel/preset-env', { targets: { ie: '11' } }]]
-        }),
-        nodeResolve({
-            extensions: ['.js', '.ts']
-        }),
-        esbuild({
-            // targets: 'es2015',
-            tsconfig: 'tsconfig.json'
-        }),
-        ts({
-            tsconfig: 'tsconfig.json'//path.resolve(__dirname, 'tsconfig.json')
-        }),
-        serve({
-            port: 3000,
-            contentBase: '', // 表示起的服务是在根目录下
-            openPage: '/public/index.html', // 打开的是哪个文件
-            open: false // 默认打开浏览器
-        }),
-        commonjs(),
-    ],
+//     plugins: [
+//         babel({
+//             babelHelpers: 'bundled',
+//             presets: [['@babel/preset-env', { targets: { ie: '11' } }]]
+//         }),
+//         nodeResolve({
+//             extensions: ['.js', '.ts']
+//         }),
+//         esbuild({
+//             // targets: 'es2015',
+//             tsconfig: 'tsconfig.json'
+//         }),
+//         ts({
+//             tsconfig: 'tsconfig.json'//path.resolve(__dirname, 'tsconfig.json')
+//         }),
+//         serve({
+//             port: 3000,
+//             contentBase: '', // 表示起的服务是在根目录下
+//             openPage: '/public/index.html', // 打开的是哪个文件
+//             open: false // 默认打开浏览器
+//         }),
+//         commonjs(),
+//     ],
 
-}
+// }
 
-module.exports = config
+// module.exports = config
 
 // 'use strict';
 
@@ -167,43 +167,51 @@ module.exports = config
 // const path = require('path')
 // const commonjs = require('@rollup/plugin-commonjs')
 
-// const banner = '//  bhlodash v' + pkg.version + '\n'
-//     + '//  https://github.com/ramda/ramda\n'
-//     + '//  (c) 2023-' + new Date().getFullYear() + ' Bryan Hui\n'
-//     + '//  Ramda may be freely distributed under the MIT license.\n';
+import { babel } from '@rollup/plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import pkg from './package.json'
+import path from 'path'
+import commonjs from '@rollup/plugin-commonjs'
+import Json from '@rollup/plugin-json'
 
-// const input = 'src/index.js';
+const banner = '//  bhlodash v' + pkg.version + '\n'
+    + '//  https://github.com/ramda/ramda\n'
+    + '//  (c) 2023-' + new Date().getFullYear() + ' Bryan Hui\n'
+    + '//  Ramda may be freely distributed under the MIT license.\n';
 
-// const config = {
-//     input: input,
-//     output: {
-//         // dir: path.resolve(__dirname, 'dist'),
-//         // file: path.resolve(__dirname, 'dist/bundle.js'),
-//         format: 'umd',
-//         name: 'L',
-//         exports: 'named',
-//         banner: banner
-//     },
-//     plugins: [
-//         babel({
-//             babelHelpers: 'bundled',
-//             presets: [['@babel/preset-env', { targets: { ie: '11' } }]]
-//         }),
-//         commonjs()
-//     ]
-// };
+const input = 'src/index.js';
 
-// if (process.env.NODE_ENV === 'production') {
-//     config.plugins.push(
-//         uglify({
-//             compress: {
-//                 pure_getters: true,
-//                 unsafe: true,
-//                 unsafe_comps: true,
-//             },
-//             warnings: false
-//         })
-//     );
-// }
+const config = {
+    input: input,
+    output: {
+        // dir: path.resolve(__dirname, 'dist'),
+        file: path.resolve(__dirname, 'dist/bundle.js'),
+        format: 'umd',
+        name: 'L',
+        exports: 'named',
+        banner: banner
+    },
+    plugins: [
+        babel({
+            babelHelpers: 'bundled',
+            presets: [['@babel/preset-env', { targets: { ie: '11' } }]]
+        }),
+        commonjs(),
+        Json()
+    ]
+};
 
-// module.exports = config;
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        uglify({
+            compress: {
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+            },
+            warnings: false
+        })
+    );
+}
+
+module.exports = config;
--- src/add.ts
+++
@@ -1,5 +0,0 @@
-const add = function (num1: number, num2: number) {
-  return num1 + num2;
-};
-
-module.exports = add;
